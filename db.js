18b35aebe7ab9127323dd8af0b2c53f23c0faa65
redhiker <LarryIrvine@verizon.net>
2016-08-11 14:38:52 -0400

    second draft..

--- db.js
+++ db.js
@@ -23,8 +23,31 @@ teamid
 description
 type (team,private)
 */
+var express = require('express');
+var sqlite3 = require('sqlite3').verbose();
+var fs = require('fs');
+
+var filename = 'scratch.db';
+var dbexists = false;
+try {
+    fs.accessSync(filename);
+    dbexists = true;
+} catch (ex) {
+    dbexists = false;
+}
+
+var db = new sqlite3.Database('test.db');
+
+
+function createDatabase(){
+    console.log('in create database');
+};
 
 function connection() {
+    if (!dbexists) {
+        createDatabase();
+    };
+
     console.log('in connection');
 };
 
